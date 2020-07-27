40603731bf5815fa92601b1c1788f4de65d7f2be
Eren FAIKOGLU <e@erenus.net>
2020-07-27 21:30:05 +0300

    Project start.

---
+++ MLImageRecognize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLImageRecognize
+//
+//  Created by Eren FAIKOGLU on 27.07.2020.
+//  Copyright © 2020 Eren FAIKOGLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
