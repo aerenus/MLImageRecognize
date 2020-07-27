539336614f7217b9f5dd88026420f8b154afbe03
Eren FAIKOGLU <e@erenus.net>
2020-07-27 23:24:56 +0300

    Project end.

--- MLImageRecognize/ViewController.swift
+++ MLImageRecognize/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var result: UILabel!
+    var chosenImg = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         image.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
-        
+        if let chosenImg = CIImage(image: image.image!) {
+            recImg(image: chosenImg)
+        }
     }
     
+    func recImg(image: CIImage){
+        result.text = "Please wait..."
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model){
+            let request = VNCoreMLRequest(model: model) { (VNReq, Error) in
+                if let results = VNReq.results as? [VNClassificationObservation]{
+                    if results.count > 0 {
+                        let topResult = results.first
+                        DispatchQueue.main.async {
+                            //let level = Int()*100
+                            let confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            let rounded = Int (confidenceLevel * 100) / 100
+                            self.result.text = "\(rounded) %, it's \(topResult?.identifier ?? "Err")"
+                        
+                    }
+                }
+            }
+                
+              
+
+    
+}
+            let handler = VNImageRequestHandler(ciImage: image)
+                          DispatchQueue.global(qos: .userInteractive).async {
+                            do { try handler.perform([request])} catch {print("err.")}
+                          }
+                }
+
+                
+            }
 }
